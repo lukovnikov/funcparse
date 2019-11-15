2db98ad9d1d661ffb5396cbd419868cc690a4596
lukovnikov <lukovnikov@outlook.com>
2019-11-15 16:53:11 +0100

    initial state setting, rnn init state refactor

--- funcparse/scripts/geoquery_geo880.py
+++ funcparse/scripts/geoquery_geo880.py
@@ -266,7 +266,7 @@ class BasicPtrGenModel(TransitionModel):
 
         if "rnn" not in x.batched_states:
             init_rnn_state = self.out_rnn.get_init_state(emb.size(0), emb.device)
-            init_rnn_state[f"{len(init_rnn_state)-1}"]["c"] = final_enc
+            # init_rnn_state[f"{len(init_rnn_state)-1}"]["c"] = final_enc
             x.batched_states["rnn"] = init_rnn_state
 
         # DONE: concat previous attention summary to emb
